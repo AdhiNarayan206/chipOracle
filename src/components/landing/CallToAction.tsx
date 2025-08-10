c78be3ccc2b4590115c039d4dd21d511dc3ca07d
[dyad] <adhinarayan206@gmail.com>
2025-08-11 00:04:39 +0530

    [dyad] Configuring app for deployment - wrote 4 file(s) + extra files edited outside of Dyad

--- src/components/landing/CallToAction.tsx
+++ src/components/landing/CallToAction.tsx
@@ -46,7 +46,7 @@ export const CallToAction = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("/api/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
